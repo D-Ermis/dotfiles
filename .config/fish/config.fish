b686c53f0ce08f8a5922859cffb55f04772ac852
D-Ermis <ermisdogukan@gmail.com>
2020-01-04 16:55:51 +0100

    add oh my fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,5 @@ set fish_greeting ""
 set -x CLICOLOR 1
 set -x LSCOLORS gxfxcxdxbxegedabagacad
 
-#set -x -U GOROOT /usr/local/Cellar/go/1.11/libexec
-#set -x -U GOPATH $WORKS_PATH/go
-#set -x -U GOBIN $GOPATH/bin
-
-set -x -U FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
+#set -x -U FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
